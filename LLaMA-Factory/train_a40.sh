5bca741effb6984b27a432a5a733f6d4da71e79a
leonz <leonz.z@outlook.com>
2025-01-04 15:42:05 +0000

    Renew the train_a40 shell script

--- LLaMA-Factory/train_a40.sh
+++ LLaMA-Factory/train_a40.sh
@@ -2,17 +2,11 @@
 
 source activate topo_qwen
 
-<<<<<<< HEAD
-export CUDA_VISIBLE_DEVICES=0,1,3,4
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 # model_name_or_path="/share/huggingface/Qwen2-VL-2B-Instruct"
 # model_name="Qwen2-VL-2B-Instruct"
-model_name_or_path="/share/huggingface/Qwen2-VL-2B-Instruct"
-=======
-export CUDA_VISIBLE_DEVICES=1
-
 model_name_or_path="/share/model/Qwen2-VL-2B-Instruct"
->>>>>>> d37fbace87fe0eefe1481f986515160256fa81c2
 model_name="Qwen2-VL-2B-Instruct"
 datasets="topo2text_v1,topo2text_v2"
 dataset_dir="data"
@@ -22,11 +16,7 @@ IFS=',' read -r -a dataset_array <<<"$datasets"
 
 for dataset in "${dataset_array[@]}"; do
     res_name="train_${dataset}_$(date +'%Y-%m-%d-%H-%M-%S')"
-<<<<<<< HEAD
-    log_file="logs/llamafactory-cli_train_${dataset}_$(date +'%Y-%m-%d-%H-%M-%S').log"
-=======
-    log_file="logs/llamafactory-cli_train_${model_name}_${dataset}_$(date +'%Y-%m-%d-%H-%M-%S').log"
->>>>>>> d37fbace87fe0eefe1481f986515160256fa81c2
+    log_file="logs/train_${model_name}_${dataset}_$(date +'%Y-%m-%d-%H-%M-%S').log"
     output_dir="$output_base_dir/$res_name"
 
     mkdir -p "$(dirname "$log_file")"
@@ -45,16 +35,12 @@ for dataset in "${dataset_array[@]}"; do
         --dataset_dir "$dataset_dir" \
         --dataset "$dataset" \
         --cutoff_len 2048 \
-        --learning_rate 5e-05 \
-        --num_train_epochs 3.0 \
+        --learning_rate 1e-04 \
+        --num_train_epochs 8.0 \
         --max_samples 100000 \
-<<<<<<< HEAD
         --per_device_train_batch_size 5 \
-=======
-        --per_device_train_batch_size 2 \
->>>>>>> d37fbace87fe0eefe1481f986515160256fa81c2
-        --gradient_accumulation_steps 8 \
-        --lr_scheduler_type cosine \
+        --gradient_accumulation_steps 4 \
+        --lr_scheduler_type cosine_with_restarts \
         --max_grad_norm 1.0 \
         --logging_steps 5 \
         --save_steps 100 \
@@ -67,9 +53,9 @@ for dataset in "${dataset_array[@]}"; do
         --trust_remote_code True \
         --ddp_timeout 180000000 \
         --optim adamw_torch \
-        --lora_rank 8 \
+        --lora_rank 16 \
         --lora_alpha 16 \
-        --lora_dropout 0 \
+        --lora_dropout 0.05 \
         --lora_target all >"$log_file" 2>&1
 
     if [ $? -eq 0 ]; then
